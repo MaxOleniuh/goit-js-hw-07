c30928762c895270b9a70a795011dbc0d9a961a8
oleniuh <maxim.oleniukh@gmail.com>
2023-03-28 11:55:28 +0100

    task-02 finish

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+function createMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+        return `
+       <li class="gallery__item">
+   <a class="gallery__link"
+        href="${original}">
+      <img class="gallery__image"
+      src="${preview}" 
+      alt="${description}"/>
+   </a>
+</li>
+`;
+    })
+    .join("");
+}
+const galleryMarkup = createMarkup(galleryItems);
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
 
-console.log(galleryItems);
+const ligthBoxGallery = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
